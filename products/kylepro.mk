b6a8b43ca055b14a8ea70b4a475cb174be63a5b0
Real man born on semtember <hieunam03@gmail.com>
2017-08-19 06:29:59 +0700

    Update

--- products/kylepro.mk
+++ products/kylepro.mk
@@ -1,3 +1,7 @@
+# Release name
+PRODUCT_RELEASE_NAME := kylepro
+
+
 # Inherit some common HEXAGON stuff.
 $(call inherit-product, vendor/hexagon/configs/common.mk)
 
@@ -5,7 +9,10 @@ $(call inherit-product, vendor/hexagon/configs/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
+$(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=kyleproxx TARGET_DEVICE=kylepro BUILD_FINGERPRINT="samsung/kyleproxx/kylepro:4.2.2/SME13A/S7580XXUAMK5:user/release-keys" PRIVATE_BUILD_DESC="kyleproxx-user 4.2.2 SME13A S7580XXUAMK5 release-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
@@ -13,4 +20,3 @@ PRODUCT_NAME := hexagon_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
-PRODUCT_CHARACTERISTICS := phone
